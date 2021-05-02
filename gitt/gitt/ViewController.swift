7dd116bdfdef767997d05341183c85d6228594fd
tassylay <tolegenasylai@gmail.com>
2021-05-02 17:15:46 +0500

    added project

---
+++ gitt/gitt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitt
+//
+//  Created by Асылай on 08.07.18.
+//  Copyright © 2021 Assylay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
